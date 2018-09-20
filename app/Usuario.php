0ab90c8ec551a3c297f7d4bbec531c9ef05c27d6
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-20 16:53:05 -0300

    Adicionando cargo no model Usuario

--- app/Usuario.php
+++ app/Usuario.php
@@ -12,7 +12,7 @@ class Usuario extends Authenticatable
     use Notifiable;
     
     protected $fillable = [
-        'nome', 'login', 'email', 'senha'
+        'nome', 'login', 'email', 'senha', 'cargo'
     ];
 
     protected $hidden = [
