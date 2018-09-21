eb02ddcabbd83992c0f8f8b759a4051a64019da0
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-21 09:35:57 -0300

    Criando arquivo policy para o model usuario

---
+++ app/Policies/UsuarioPolicy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Usuario;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UsuarioPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the usuario.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Usuario  $usuario
+     * @return mixed
+     */
+    public function view(User $user, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create usuarios.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the usuario.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Usuario  $usuario
+     * @return mixed
+     */
+    public function update(User $user, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the usuario.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Usuario  $usuario
+     * @return mixed
+     */
+    public function delete(User $user, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the usuario.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Usuario  $usuario
+     * @return mixed
+     */
+    public function restore(User $user, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the usuario.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Usuario  $usuario
+     * @return mixed
+     */
+    public function forceDelete(User $user, Usuario $usuario)
+    {
+        //
+    }
+}
