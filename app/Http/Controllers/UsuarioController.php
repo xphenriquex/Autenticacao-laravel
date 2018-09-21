71690e22ff57c66d5208b40a1f97b87a2e6b9536
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-21 09:34:02 -0300

    Adc metodo atualizar

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Usuario;
 
 class UsuarioController extends Controller
@@ -15,6 +16,21 @@ class UsuarioController extends Controller
     public function editar($id){
         $usuario = Usuario::find($id);
         return view('usuarios.editar', compact('usuario'));
+    }
+
+    public function atualizar(Request $request, $id){
+        $dados = $request->all();
+        $usuario = Usuario::find($id);
+
+        if(!$dados['senha']){
+            $dados['senha'] = $usuario->senha;
+            $usuario->update($dados);
+        }else{
+            $dados['senha'] = Hash::make($dados['senha']);
+            $usuario->update($dados);
+        }
+
+        return redirect()->route('listar');
     }
 
     public function registrar(){
